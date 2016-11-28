9416fd01ab4d1c7e31543d132e1502700883afe7
mkrai <mkray3.0@hotmail.com>
2016-11-28 14:59:15 -0600

    first commit

---
+++ TutorialPageViewController/TutorialPageViewController.swift
@@ -0,0 +1,87 @@
+//
+//  TutorialPageViewController.swift
+//  TutorialPageViewController
+//
+//  Created by Oscar Hiram Alvarez Campos on 28/11/16.
+//  Copyright © 2016 Mk-Projects. All rights reserved.
+//
+
+import UIKit
+
+class TutorialPageViewController: UIPageViewController {
+    
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newColoredViewController(color: "Green"),
+                self.newColoredViewController(color: "Red"),
+                self.newColoredViewController(color: "Blue")]
+    }()
+    
+    private func newColoredViewController(color: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil) .
+            instantiateViewController(withIdentifier: "\(color)ViewController")
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dataSource = self
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
+        
+    }
+}
+
+// MARK: UIPageViewControllerDataSource
+
+extension TutorialPageViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else{
+        return nil
+            }
+        let previousIndex = viewControllerIndex - 1
+        guard previousIndex >= 0 else {
+        return nil
+        }
+        guard orderedViewControllers.count > previousIndex else{
+        return nil
+        }
+        return orderedViewControllers[previousIndex]
+    }
+    
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController)else{
+        return nil
+        }
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else{
+        return nil
+        }
+        guard orderedViewControllersCount > nextIndex else {
+        return nil
+        }
+        return orderedViewControllers[nextIndex]
+    }
+}
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
